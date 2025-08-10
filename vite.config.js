ff4a1394bf60104e0f6aa503e28ad376c93eeba6
Anais <anais.shakhidi@gmail.com>
2025-08-11 02:39:02 +0400

    Hello

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/UKConferences/'
+  base: '/UKConferences/', // Change this to your repo name for GitHub Pages
 })
